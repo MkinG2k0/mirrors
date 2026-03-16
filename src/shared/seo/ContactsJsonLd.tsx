62af7aa44b0ff3b98dd0a05d26e22b3398855dc4
mk <kamai122000@mail.ru>
2026-03-16 13:48:47 +0300

    Remove unnecessary "use client" directives from ContactsJsonLd and FaqJsonLd components; add priceRange to local business JSON-LD data

--- src/shared/seo/ContactsJsonLd.tsx
+++ src/shared/seo/ContactsJsonLd.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { JsonLd, getLocalBusinessJsonLd } from "./JsonLd";
 
 interface ContactsJsonLdProps {
--- src/shared/seo/JsonLd.tsx
+++ src/shared/seo/JsonLd.tsx
@@ -74,4 +74,3 @@ export function getLocalBusinessJsonLd(siteUrl: string) {
     sameAs: [INSTAGRAM_URL],
   };
 }
-
